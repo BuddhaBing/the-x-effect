2b1692e254117970fed1094b355a1cf2b2019f79
Rob Brentnall <rob.brentnall@gmail.com>
2017-04-01 17:54:38 +0100

    sessions feature tests passing

--- spec/feature/user_features_spec.rb
+++
@@ -1,5 +0,0 @@
-feature 'Sign up' do
-  it 'user can sign up for an account' do
-    expect{sign_up}.to change{User.count}.by(1)
-  end
-end
--- spec/helpers/user_helpers.rb
+++
@@ -1,8 +0,0 @@
-def sign_up
-  visit '/users/sign_up'
-  fill_in :user_username, with: "test"
-  fill_in :user_email, with: "test@testing.com"
-  fill_in :user_password, with: "strongpassword"
-  fill_in :user_password_confirmation, with: "strongpassword"
-  click_button "Sign up"
-end
