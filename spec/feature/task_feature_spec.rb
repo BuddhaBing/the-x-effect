dc670c4a34253fc31dea783067949977f3d4c0a6
Rob Brentnall <rob.brentnall@gmail.com>
2017-04-02 18:13:26 +0100

    refactored tests

--- spec/feature/task_feature_spec.rb
+++ spec/feature/task_feature_spec.rb
@@ -12,27 +12,21 @@ feature 'Creating a task' do
       expect(page).not_to have_link "New Task"
     end
   end
-  context 'validity' do
+  context 'checking validity' do
     before do
       sign_up
     end
-    feature 'checking name is not blank' do
-      it 'does not allow to create a task with no name' do
-        new_task(nil)
-        expect(page).to have_content("Name can't be blank")
-      end
+    it 'does not allow to create a task with no name' do
+      new_task(nil)
+      expect(page).to have_content("Name can't be blank")
     end
-    feature 'checking valid start date' do
-      it 'does not allow to create a task with start date in the the past' do
-        new_task("Exercise", "01/01/2017")
-        expect(page).to have_content("Start date can't be in the past")
-      end
+    it 'does not allow to create a task with start date in the the past' do
+      new_task("Exercise", "01/01/2017")
+      expect(page).to have_content("Start date can't be in the past")
     end
-    feature 'checking valid end date' do
-      it 'does not allow to create a task with end date prior to the start date' do
-        new_task("Exercise", "02/01/3017", "01/01/3017")
-        expect(page).to have_content("End date can't be before the start date")
-      end
+    it 'does not allow to create a task with end date prior to the start date' do
+      new_task("Exercise", "02/01/3017", "01/01/3017")
+      expect(page).to have_content("End date can't be before the start date")
     end
   end
 end
