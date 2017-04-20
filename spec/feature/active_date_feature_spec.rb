d24a45e8b3be1189cc99d3096e5b29e5677e226a
Rob Brentnall <rob.brentnall@gmail.com>
2017-04-20 14:21:28 +0100

    improved tests for marking dates/tasks as complete and missed

--- spec/feature/active_date_feature_spec.rb
+++ spec/feature/active_date_feature_spec.rb
@@ -6,10 +6,14 @@ feature 'marking a task as complete' do
   end
   it 'user can mark a task as complete for a particular date' do
     click_link "day-1-complete"
+    active_date = Task.first.active_dates.first
     expect(page).to have_content("Awesome! Keep it up!")
+    expect(active_date.completed).to be true
   end
   it 'user can mark a task as missed for a particular date' do
     click_link "day-1-missed"
+    active_date = Task.first.active_dates.first
     expect(page).to have_content("Aw shucks!")
+    expect(active_date.completed).to be false
   end
 end
